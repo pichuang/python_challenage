19dd1dc8bf2a2c5b824d6670d126c07d27cd2402
Roan Huang <pichuang@cs.nctu.edu.tw>
2015-07-02 22:39:45 +0800

    Complete Challenge 13

---
+++ 13.py
@@ -0,0 +1,21 @@
+__author__ = 'roan'
+
+
+"""
+http://www.pythonchallenge.com/pc/phonebook.php
+Use xmlrpc
+"""
+
+from xmlrpc import client
+
+
+phonebook = client.ServerProxy('http://www.pythonchallenge.com/pc/phonebook.php')
+
+# List phonebook method
+print(phonebook.system.listMethods())
+
+method = phonebook.system.listMethods()[0]
+
+print(phonebook.system.methodHelp(method))
+print(phonebook.system.methodSignature(method))
+print(phonebook.phone("Bert"))  # "Bert is evil", level 12
